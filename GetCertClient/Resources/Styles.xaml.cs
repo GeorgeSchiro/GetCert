e71f16d19ac2f98da784b744031816e8e29c400f
Geo <GeoCode@Schiro.name>
2019-05-11 22:02:10 -0400

    Initial commit.

---
+++ GetCertClient/Resources/Styles.xaml.cs
@@ -0,0 +1,16 @@
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace GetCert
+{
+    partial class Styles : ResourceDictionary
+    { 
+        public Styles()
+        {
+            InitializeComponent();
+        }
+
+        // Event handlers go here.
+    }
+}
